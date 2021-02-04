e90cdd9c82770f306fc64f74216a21803ccbd4ef
Emin Hamzagic <hamzagicemin4@gmail.com>
2021-02-04 11:51:44 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Kursevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader f = new StreamReader("kursevi.txt");
+                Kurs k = new Kurs();
+                k.CitajIzFajla(f);
+                Kurs najskuplji = new Kurs(k);
+                while (!f.EndOfStream)
+                {
+                    k.CitajIzFajla(f);
+                    if (k.SkupljiOd(najskuplji))
+                        najskuplji.Kopiraj(k);
+                }
+                f.Close();
+                textBox1.Text = najskuplji.ToString();
+            }
+            catch(Exception izuzetak)
+            {
+                MessageBox.Show(izuzetak.Message, "Greska");
+            }
+        }
+    }
+}
---
+++ Kurs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kursevi
+{
+    class Kurs
+    {
+        string naziv;
+        int brCasova, ukupnaCena;
+        public Kurs()
+        {
+            naziv = string.Empty;
+            brCasova = ukupnaCena = 0; 
+        }
+        public Kurs(string naziv, int brCasova, int ukupnaCena)
+        {
+            if (brCasova >= 0 && ukupnaCena >= 0)
+            {
+                this.naziv = naziv;
+                this.brCasova = brCasova;
+                this.ukupnaCena = ukupnaCena;
+            }
+            else
+                throw new Exception("Br. casova i cena moraju da budu >=0");
+        }
+        public Kurs(Kurs k)
+        {
+            naziv = k.naziv;
+            brCasova = k.brCasova;
+            ukupnaCena = k.ukupnaCena;
+        }
+        public void CitajIzFajla(StreamReader f)
+        {
+            naziv = f.ReadLine();
+            brCasova = Convert.ToInt32(f.ReadLine());
+            ukupnaCena = Convert.ToInt32(f.ReadLine());
+            if (brCasova < 0 || ukupnaCena < 0)
+                throw new Exception("Br. casaova i cena moraju da budu >=0");
+
+        }
+        public void Kopiraj(Kurs k)
+        {
+            naziv = k.naziv;
+            brCasova = k.brCasova;
+            ukupnaCena = k.ukupnaCena;
+        }
+        public override string ToString()
+        {
+            return naziv + " - " + brCasova + " casova, " + ukupnaCena + "din";
+        }
+        public double CenaPoCasu()
+        {
+            return (double)ukupnaCena / brCasova;
+        }
+        public bool SkupljiOd(Kurs k)
+        {
+            return CenaPoCasu() > k.CenaPoCasu();
+        }
+    }
+}
